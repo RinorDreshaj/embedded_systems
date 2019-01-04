ae5f632856e773805915a9ecafbf9eaba6e66481
Rinor <rinor@Rinors-MacBook-Pro.local>
2019-01-05 00:33:22 +0100

    ADD: lib folder with UART, CommProtocol and UART_PC

---
+++ lib/com_protocol/CommProtocol.h
@@ -0,0 +1,13 @@
+#ifndef COMMPROTOCOL_H_
+#define COMMPROTOCOL_H_
+
+unsigned char	COMM_IsHeader(unsigned char);
+unsigned char	COMM_IsWrite(unsigned char);
+unsigned char	COMM_GetField(unsigned char);
+unsigned char	COMM_GetWriteHeader(unsigned char, unsigned int);
+unsigned char	COMM_GetWritePayload(unsigned int);
+unsigned char	COMM_GetReadHeader(unsigned char);
+unsigned int	COMM_GetPayloadHigh(unsigned char);
+unsigned int	COMM_GetPayloadLow(unsigned char);
+
+#endif
---
+++ lib/uart_pc/UART_PC.c
@@ -0,0 +1,36 @@
+#define F_CPU   14745600
+#include "UART_PC.h"
+#include <avr/io.h>
+
+void UART_PC_Init(unsigned int baudrate) {
+	
+	UCSR1B = (1<<RXEN1) | (1<<TXEN1);
+	UCSR1C = (1<<UCSZ11) | (1<<UCSZ10);
+	
+	unsigned long int ubrrValue = F_CPU / (16UL * baudrate) - 1;
+	
+	UBRR1H = (ubrrValue >> 8) & 0xFF;
+	UBRR1L = ubrrValue & 0xFF;
+}
+
+unsigned char UART_PC_DataAvailable() {
+	
+	return UCSR1A & (1<<RXC1);
+}
+
+unsigned char UART_PC_Read() {
+	
+	return UDR1;
+}
+
+unsigned char UART_PC_ReadBlocking() {
+	
+	while(!(UCSR1A & (1<<RXC1)));
+	return UDR1;
+}
+
+void UART_PC_Write(unsigned char data) {
+	
+	while(!TXC1);
+	UDR1 = data;
+}
